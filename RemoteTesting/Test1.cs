189628140f9c343d4234bdaf26403852240e4c11
Ossian Edström <ossian.edstrom@epplussoftware.com>
2026-09-25 11:22:42 +0200

    Initial commit

---
+++ RemoteTesting/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace RemoteTesting
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var myNumber = 2;
+            Assert.AreEqual(2, myNumber);
+        }
+    }
+}
